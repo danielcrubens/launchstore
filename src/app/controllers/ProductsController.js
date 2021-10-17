688bec2be82922f79bd247480a358007097fb14d
danielcrubens <dcr.tecnologi@gmail.com>
2021-10-16 22:53:09 -0300

    funcionalidade  de atualização de produtos :bulb:

--- src/app/controllers/ProductsController.js
+++ src/app/controllers/ProductsController.js
@@ -1,3 +1,4 @@
+const { formatPrice } = require("../../lib/utils");
 const Category = require("../models/Category");
 const Product = require("../models/Product");
 
@@ -24,8 +25,33 @@ module.exports = {
     }
     let results = await Product.create(req.body);
     const productId = results.rows[0].id;
+    return res.redirect(`/products/${productId}`);
+  },
+  async edit(req, res) {
+    let results = await Product.find(req.params.id);
+    const product = results.rows[0];
+    product.old_price = formatPrice(product.old_price);
+    product.price = formatPrice(product.price);
+    if (!product) return res.send("Product not found!");
     results = await Category.all();
     const categories = results.rows;
-    return res.render("products/create.njk", { productId, categories });
+    return res.render("products/edit.njk", { product, categories });
   },
+  async put(req, res){
+    const keys = Object.keys(req.body);
+
+    for (let key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields");
+      }
+    }
+    req.body.price = req.body.price.replace(/\D/g,"")
+    
+    if(req.body.old_price != req.body.price){
+      const oldProduct = await Product.find(req.body.id)
+      req.body.old_price = oldProduct.rows[0].price
+    }
+    await Product.update(req.body)
+    return res.redirect(`/products/${req.body.id}/edit`)
+  }
 };
