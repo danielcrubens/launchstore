4835fe93fae714b1215d6edb0113e0af353cc582
danielcrubens <dcr.tecnologi@gmail.com>
2022-02-19 14:52:27 -0300

    funcionalidade mudando galeria de imagens com clique :white_check_mark:

--- public/scripts.js
+++ public/scripts.js
@@ -112,3 +112,14 @@ const PhotosUpload = {
         photoDiv.remove();
     }
 };
+/* FUNCIONALIDADE DA GALERIA DE IMAGENS */
+const ImageGallery ={
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('gallery-preview img'),
+    setImage(e){
+        const {target}= e
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+        target.classList.add('active')
+        ImageGallery.highlight.src = target.src
+    }
+}
