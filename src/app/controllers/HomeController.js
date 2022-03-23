7609259db7f71605075d288b26f6d583fc891b9e
danielcrubens <dcr.tecnologi@gmail.com>
2022-03-23 19:44:17 -0300

    estruturando dados para o front-end :fire:

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -7,5 +7,22 @@ module.exports = {
         let results = await Product.all()
         const products = results.rows
         if(!products) return res.send("Products not foud!")
+
+        async function getImage(productId){
+        let results = await Product.files(product.id)
+        const files = results.rows
+        files = files.map(file => 
+            `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+        return files[0]
     }
+    const productsPromise = products.map(product=>{
+        product.img =await getImage(product)
+        product.oldPrice = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+        return product
+    })
+    const lastAdded = await Promise.all(productsPromise)
+    return res.render("home/index",{products:lastAdded})
+    }
+    
 }
