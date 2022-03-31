53a71452cdf4f7f1b869e526df94cefe123f0add
danielcrubens <dcr.tecnologi@gmail.com>
2022-03-31 20:21:35 -0300

    validando registro de usuário no backend :rocket:

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,23 +1,32 @@
 const User = require('../models/User')
 module.exports = {
- registerForm(req,res){
-     return res.render("user/register")
- },
- post(req,res){
-     //check if has all fields
-     const keys = Object.keys(req.body)
+    registerForm(req, res) {
+        return res.render("user/register")
+    },
+    async post(req, res) {
+        //check if has all fields
+        const keys = Object.keys(req.body)
 
-     for (let key of keys) {
-       if (req.body[key] == "") {
-         return res.send('Please, fill all fields')
-       }
-     }
-     //check if user exists
-     const {email, cpf_cnpj} =req.body
-     const user = await User.findOne({
-         where: {email},
-         or:{cpf_cnpj}
-     }) 
+        for (let key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields')
+            }
+        }
+        //check if user exists
+        let { email, cpf_cnpj, password, passwordRepeat } = req.body
+        cpf_cnpj = cpf_cnpj.replace(/\D/g,"")
+
+        const user = await User.findOne({
+            where: { email },
+            or: { cpf_cnpj }
+        })
+        if(user) return res.send('Users exists')
+                //check if password match
+        if(password != passwordRepeat)
+        return res.send('Password Mismatch')
+
+        return res.send('passed!')
+
      
  }
 }
