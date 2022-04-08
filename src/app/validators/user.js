2d794c8f85d31cfa4130f8337c6db8253b664e89
danielcrubens <dcr.tecnologi@gmail.com>
2022-04-08 18:34:09 -0300

    customizando mensagens de alerta :fire:

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,31 +1,37 @@
 const User = require('../models/User')
 
-async function post(req,res, next){
-  //check if has all fields
-  const keys = Object.keys(req.body)
+async function post(req, res, next) {
+    //check if has all fields
+    const keys = Object.keys(req.body)
 
-  for (let key of keys) {
-      if (req.body[key] == "") {
-          return res.send('Please, fill all fields')
-      }
-  }
-  //check if user exists
-  let { email, cpf_cnpj, password, passwordRepeat } = req.body
-  cpf_cnpj = cpf_cnpj.replace(/\D/g,"")
- 
-  const user = await User.findOne({
-      where: { email },
-      or: { cpf_cnpj }
-  })
-  if(user) return res.render('user/register',{
-      user: req.body,
-      error:'Usuário já cadastrado.'
-  })
-          //check if password match
-  if(password != passwordRepeat)
-  return res.send('Password Mismatch')
+    for (let key of keys) {
+        if (req.body[key] == "") {
+            return res.render('user/register', {
+                user: req.body,
+                error: 'Por favor,preencha todos os campos.'
+            })
+        }
+    }
+    //check if user exists
+    let { email, cpf_cnpj, password, passwordRepeat } = req.body
+    cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+
+    const user = await User.findOne({
+        where: { email },
+        or: { cpf_cnpj }
+    })
+    if (user) return res.render('user/register', {
+        user: req.body,
+        error: 'Usuário já cadastrado.'
+    })
+    //check if password match
+    if (password != passwordRepeat) 
+    return res.render('user/register', {
+        user: req.body,
+        error: 'A senha e a repetição da senha estão incorretas.'
+    })
     next()
 }
-module.exports ={
+module.exports = {
     post
 }
