a4689c0a2429094c64d8132a89ab3fa5c2242174
danielcrubens <dcr.tecnologi@gmail.com>
2022-04-12 10:03:45 -0300

    controller de atualização do  usuário :fire:

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,6 +20,29 @@ module.exports = {
 
     }, 
     async update(req, res) {
+        try{
+            let {name, email, cpf_cnpj,cep, address} = req.body
 
+            cpf_cnpj = cpf_cnpj.replace(/\D/g,"")
+            cep = cep.replace(/\D/g,"")
+
+            await User.update(user.id,{
+                name,
+                email,
+                cpf_cnpj,
+                cep,
+                address
+            })
+
+            return res.render("user/index",{
+                success:"Conta atualizada com sucesso!"
+            })
+        }catch(err){
+            console.error(err)
+            return res.render("user/index",{
+                error:"Algum erro aconteceu!"
+
+            })
+        }
     }
 }
