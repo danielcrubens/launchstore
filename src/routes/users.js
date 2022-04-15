7a3b64e274cd23a979acd516b229d33d228e6238
danielcrubens <dcr.tecnologi@gmail.com>
2022-04-15 19:50:46 -0300

    redirecionamento de usuários cadastrados :white_check_mark:

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,15 +5,15 @@ const UserController = require("../app/controllers/UserController")
 const UserValidator = require('../app/validators/user')
 const SessionValidator = require('../app/validators/session')
 
-const {isLoggedRedirectToUsers}= require('../app/middlewares/session')
+const { isLoggedRedirectToUsers, onlyUsers } = require('../app/middlewares/session')
 
 
 
 
 //login/logout
- routes.get('/login',isLoggedRedirectToUsers, SessionController.loginForm)
-routes.post('/login',SessionValidator.login, SessionController.login)
-routes.post('/logout', SessionController.logout) 
+routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
+routes.post('/login', SessionValidator.login, SessionController.login)
+routes.post('/logout', SessionController.logout)
 
 //login/logout
 /* routes.get('/forgot-password', SessionController.forgotForm)
@@ -23,10 +23,10 @@ routes.post('/password-reset', SessionController.reset) */
 
 // user register UserController
 routes.get('/register', UserController.registerForm)
- routes.post('/register',UserValidator.post, UserController.post)
+routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/',UserValidator.show, UserController.show)
-routes.put('/',UserValidator.update, UserController.update)
+routes.get('/', onlyUsers, UserValidator.show, UserController.show)
+routes.put('/', UserValidator.update, UserController.update)
 /*routes.delete('/register', UserController.delete) */
 
 
