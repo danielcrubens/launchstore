cc47a4015d8d38aeae2f34c500c7763e61767d68
danielcrubens <dcr.tecnologi@gmail.com>
2022-04-15 21:35:45 -0300

    enviando email com nodemailer :white_check_mark:

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,31 +1,49 @@
 const crypto = require('crypto')
-const User = require('../models/User')
+const mailer = require('../../lib/mailer')
 module.exports = {
-    loginForm(req,res){
+    loginForm(req, res) {
         return res.render("session/login")
     },
-    login(req,res){
+    login(req, res) {
         req.session.userId = req.user.id
         return res.redirect("/users")
     },
-    logout(req,res){
+    logout(req, res) {
         req.session.destroy()
         return res.redirect("/")
     },
-    forgotForm(req,res){
+    forgotForm(req, res) {
         return res.render("session/forgot-password")
     },
 
-    forgot(req, res){
-         const user = req.user
+    forgot(req, res) {
+        const user = req.user
         //um token para esse usuário
-        const token =  crypto.randomBytes(20).toLocaleString("hex")
+        const token = crypto.randomBytes(20).toLocaleString("hex")
         //criar ema expiração
         let now = new Date()
-        now = now.setHours(now.getHours()+1)
-        await  User.update(user.id,{
+        now = now.setHours(now.getHours() + 1)
+
+        await User.update(user.id, {
             reset_token: token,
-            reset_token_expires:now
+            reset_token_expires: now
+        })
+        // enviar um email com  um link de recuperação de senha
+        await mailer.sendMail({
+            to: user.email,
+            from: 'no-replay@launchstore.com.br',
+            subject:'Recuperação de senha',
+            html:`<h2> Perdeu a chave?</h2>
+            <p> Não se preocupe, clique no link abaixo para recuperar sua senha</p>
+            < href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
+            RECUPERAR SENHA
+            <a/>
+            
+            `
+        })
+        //avisar o usuário que enviamos o email
+        return res.render("session/forgot-password",{
+            success: "Verifique seu email para resetar sua senha!"
         })
 
     }
---
+++ src/lib/mailer.js
@@ -0,0 +1,8 @@
+module.exports = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "067d0065d809b1",
+      pass: "6287421543ae39"
+    }
+  });
