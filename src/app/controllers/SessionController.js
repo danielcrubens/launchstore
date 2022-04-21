5f0a9ab69a633383c96ba18202dddcdbb6db9187
danielcrubens <dcr.tecnologi@gmail.com>
2022-04-21 12:36:09 -0300

    criando nova senha  :white_check_mark:

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const hash = require('bcryptjs')
 const crypto = require('crypto')
 const mailer = require('../../lib/mailer')
 module.exports = {
@@ -16,61 +17,79 @@ module.exports = {
     forgotForm(req, res) {
         return res.render("session/forgot-password")
     },
-    async forgot(req, res) {
+        async forgot(req, res) {
+           const user = req.user
+           try {
+   
+           } catch (err) {
+               console.error(err)
+               return res.render("session/forgot-password", {
+                   error: "Erro inesperado, tente novamente"
+               })
+           }
+   
+           //um token para esse usuário
+           const token = crypto.randomBytes(20).toString("hex")
+           //criar ema expiração
+           let now = new Date()
+           now = now.setHours(now.getHours() + 1)
+   
+           await User.update(user.id, {
+               reset_token: token,
+               reset_token_expires: now
+           })
+           // enviar um email com  um link de recuperação de senha
+           await mailer.sendMail({
+               to: user.email,
+               from: 'no-replay@launchstore.com.br',
+               subject: 'Recuperação de senha',
+               html: `<h2> Perdeu a chave?</h2>
+               <p> Não se preocupe, clique no link abaixo para recuperar sua senha</p>
+               <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
+               RECUPERAR SENHA
+               <a/>
+               </p>
+               
+               `
+           })
+           //avisar o usuário que enviamos o email
+           return res.render("session/forgot-password", {
+               success: "Verifique seu email para resetar sua senha!"
+           })
+   
+       }, 
+
+  
+    resetForm(req, res) {
+        return res.render("session/password-reset", { token: req.query.token })
+    },
+    async reset(req, res) {
         const user = req.user
-        try{
+        const { password, token } = req.body
+        try {
+            //cria um novo hash de senha
+            const newPassword = await hash(password, 8)
+            // atualiza o usuário
+            await User.update(user.id, {
+                password: newPassword,
+                reset_token: "",
+                reset_token_expires: "",
+            })
+            //avisa o usuário que ele tem uma nova senha
+            return res.render("session/login", {
+                user: req.body,
+                success: "Senha atualizada! Faça o seu login"
+            })
 
-        }catch(err){
+
+        } catch (err) {
             console.error(err)
-            return res.render("session/forgot-password",{
+
+            return res.render("session/password-reset", {
+                user: req.body,
+                token,
                 error: "Erro inesperado, tente novamente"
             })
         }
-    
-        //um token para esse usuário
-        const token = crypto.randomBytes(20).toString("hex")
-        //criar ema expiração
-        let now = new Date()
-        now = now.setHours(now.getHours() + 1)
-
-        await User.update(user.id, {
-            reset_token: token,
-            reset_token_expires: now
-        })
-        // enviar um email com  um link de recuperação de senha
-        await mailer.sendMail({
-            to: user.email,
-            from: 'no-replay@launchstore.com.br',
-            subject:'Recuperação de senha',
-            html:`<h2> Perdeu a chave?</h2>
-            <p> Não se preocupe, clique no link abaixo para recuperar sua senha</p>
-            <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
-            RECUPERAR SENHA
-            <a/>
-            
-            `,
-        })
-        //avisar o usuário que enviamos o email
-        return res.render("session/forgot-password",{
-            success: "Verifique seu email para resetar sua senha!"
-        })
-
-    },
-
-    resetForm(req,res){
-        return res.render("session/password-reset",{token:req.query.token})
-    },
-     reset(req,res){
-        const {email, paassword,paasswordRepeat, token} = req.body
-        try{
-//procuras usuário
-
-        }catch(err){
-            console.error(err)
-
-            return res.render("session/password-reset",{
-                error: "Erro inesperado, tente novamente"
-            })
-        }
-    } 
+    }
 }
