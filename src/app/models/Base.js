3c98c596ed226651fdbe4df43504e591dc1a2d0e
danielcrubens <dcr.tecnologi@gmail.com>
2022-04-29 19:41:28 -0300

    criando base model :fire:

---
+++ src/app/models/Base.js
@@ -0,0 +1,68 @@
+const db = require('../../config/db')
+
+const Base = {
+    init({ table }) {
+        if (!table) throw new Error('Invalid Params')
+
+        this.table = table
+        return this
+    },
+    async findOne(filters) {
+        let query = `SELECT * FROM ${this.table}`
+
+        Object.keys(filters).map(key => {
+            query = `${query}
+     ${key}
+     `
+            Object.keys(filters[key]).map(field => {
+                query = `${query} ${field} = '${filters[key][field]}'`
+            })
+        })
+        const results = await db.query(query)
+        return results.rows[0]
+    },
+    async create(fields) {
+        try {
+            let keys = [],
+                values = []
+
+            Object.keys(fields).map(key => {
+                keys.push(key)
+                values.push(fields[key])
+            })
+
+            const query = `INSERT INTO ${this.table}(${keys.join(',')})
+            VALUES (${values.join(',')})
+            RETURNING id`
+
+            const results = await db.query(query)
+            return results.rows[0].id
+
+        } catch (error) {
+            console.error(error);
+        }
+    },
+     update(id, fields) {
+        try {
+            let update = []
+            Object.keys(fields).map(key=> {
+
+                //category_id=($1)
+                const line = `${key}= '${fields[key]}'`
+                update.push(line)
+          })
+
+            let query = `UPDATE ${this.table} SET
+            ${update.join(',')}WHERE id = ${id}
+            `
+            return db.query(query)
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    delete(id) {
+        return db.query("DELETE FROM products WHERE id = $1", [id])
+      },
+
+}
+module.exports = Base
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,24 +1,17 @@
-const db = require("../../config/db")
+/* const db = require("../../config/db")
 const { hash } = require('bcryptjs')
 const fs = require('fs')
 const Product = require('../models/Product')
+const Bse = require('./Base')
+const { init } = require("browser-sync") */
+const Base = require("./Base")
 
-module.exports = {
-    async findOne(filters) {
-        let query = "SELECT * FROM users"
+Base.init({table: 'users'})
 
-        Object.keys(filters).map(key => {
-            query = `${query}
-         ${key}
-         `
-            Object.keys(filters[key]).map(field => {
-                query = `${query} ${field} = '${filters[key][field]}'`
-            })
-        })
-        const results = await db.query(query)
-        return results.rows[0]
-    },
-    async create(data) {
+const User ={
+ 
+    ...Base,
+ /*    async create(data) {
         try {
             const query = `
             INSERT INTO users (
@@ -92,5 +85,7 @@ module.exports = {
             })
         })
 
-    }
+    } */
 }
+console.log(User)
+module.exports = User
