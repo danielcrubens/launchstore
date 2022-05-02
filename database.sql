b63ac069551ac4c3f48e035c8eaf964aaaaa61e2
danielcrubens <dcr.tecnologi@gmail.com>
2022-05-02 18:28:18 -0300

    seeds de produtos e imagens :fire:

--- database.sql
+++ database.sql
@@ -101,3 +101,13 @@ ADD CONSTRAINT files_product_id_fkey
 FOREIGN KEY ("product_id")
 REFERENCES "products" ("id")
 ON DELETE CASCADE;
+
+-- to run seeds
+DELETE FROM users;
+DELETE FROM products;
+DELETE FROM files;
+
+-- restart sequence auto increment from tables ids
+ALTER SEQUENCE products_id_seq RESTART WITH 1;
+ALTER SEQUENCE user_id_seq RESTART WITH 1;
+ALTER SEQUENCE files_id_seq RESTART WITH 1;
