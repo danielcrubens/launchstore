d9623857a514c8d15b11e1f02c6aeb7243df9886
danielcrubens <dcr.tecnologi@gmail.com>
2022-05-14 13:19:57 -0300

    deletando itens do  carrinho :white_check_mark:

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -49,7 +49,7 @@ const Cart = {
     },
     removeOne(productId) {
         //pegar o item do carrinho
-        const inCart = this.items.find(item => item.product.id == productId);
+        const inCart = this.getCartItem(productId);
 
         if (!inCart) return this;
         //atualizar o item
@@ -71,7 +71,22 @@ const Cart = {
         return this;
     },
     delete(productId) {
+        //pegar item do carrinho
 
+        const inCart = this.getCartItem(productId);
+        if (!inCart) return this;
+
+        if (this.items.length > 0) {
+            this.total.quantity -= inCart.quantity;
+            this.total.price -= (inCart.product.price * inCart.quantity);
+            this.total.formattedPrice = formatPrice(this.total.price);
+        }
+
+        this.items = this.items.filter(item => inCart.product.id != item.product.id);
+        return this;
+    },
+    getCartItem(productId) {
+        return this.items.find(item => item.product.id == productId);
     }
 }
 module.exports = Cart
