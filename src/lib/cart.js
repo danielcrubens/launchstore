5cbbe5eacb2f5ef0be618a51c20bc8c7408e6958
danielcrubens <dcr.tecnologi@gmail.com>
2022-05-14 12:45:52 -0300

    adicionado itens a o carrinho :white_check_mark:

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -2,23 +2,56 @@ const { formatPrice } = require('./utils');
 // carrinho fica guardado na sessão (req.session)
 
 
-const Cart ={
-    init(oldCart){
+const Cart = {
+    init(oldCart) {
         if (oldCart) {
             this.items = oldCart.items
             this.total = oldCart.total
-        }else{
-            this.items =[]
-            this.total ={
-                quantity:0,
-                price:0,
-                formatPrice: formatPrice(0)
+        } else {
+            this.items = []
+            this.total = {
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0)
             }
         }
         return this
     },
-    addOne(product){},
-    removeOne(productId){},
-    delete(productId){}
+    addOne(product) {
+        //ver se o produto já existe no carrinho
+        let inCart = this.getCartItem(product.id);
+
+        if (!inCart) {
+            inCart = {
+                product: {
+                    ...product,
+                    formattedPrice: formatPrice(product.price)
+                },
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0)
+            };
+
+            this.items.push(inCart);
+        }
+
+        if (inCart.quantity >= product.quantity) return this;
+        //update item
+        inCart.quantity++;
+        inCart.price = inCart.product.price * inCart.quantity;
+        inCart.formattedPrice = formatPrice(inCart.price);
+        //update cart
+        this.total.quantity++;
+        this.total.price += inCart.product.price;
+        this.total.formattedPrice = formatPrice(this.total.price);
+
+        return this;
+    },
+    removeOne(productId) {
+
+    },
+    delete(productId) {
+
+    }
 }
 module.exports = Cart
