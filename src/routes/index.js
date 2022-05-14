1556259358270d0f3cc3bdb2ea72c869315513ff
danielcrubens <dcr.tecnologi@gmail.com>
2022-05-14 15:22:29 -0300

    estrutura html do carrinho :white_check_mark:

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const routes = express.Router()
 const HomeController = require("../app/controllers/HomeController")
 const products = require("./products")
 const users = require("./users")
+const cart = require("./cart")
+
 
 //Home
 routes.get("/", HomeController.index)
@@ -10,6 +12,9 @@ routes.get("/", HomeController.index)
 routes.use("/products", products)
 //Users
 routes.use("/users",users)
+//Cart
+routes.use("/cart",cart)
+
 //Alias
 routes.get("/ads/create", function (req, res) {
   return res.redirect("/products/create")
