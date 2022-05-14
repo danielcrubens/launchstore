65842a5284fa8cdd9871e07fd8381af895d8dfa2
danielcrubens <dcr.tecnologi@gmail.com>
2022-05-14 13:04:07 -0300

    removendo item do  carrinho :white_check_mark:

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -48,7 +48,27 @@ const Cart = {
         return this;
     },
     removeOne(productId) {
+        //pegar o item do carrinho
+        const inCart = this.items.find(item => item.product.id == productId);
 
+        if (!inCart) return this;
+        //atualizar o item
+        inCart.quantity--;
+        inCart.price = inCart.product.price * inCart.quantity;
+        inCart.formattedPrice = formatPrice(inCart.price);
+        //atualizar o carrinho
+        this.total.quantity--;
+        this.total.price -= inCart.product.price;
+        this.total.formattedPrice = formatPrice(this.total.price);
+
+        if (inCart.quantity < 1) {
+            this.items = this.items.filter(item =>
+                item.product.id != inCart.product.id);
+
+            return this;
+        }
+
+        return this;
     },
     delete(productId) {
 
